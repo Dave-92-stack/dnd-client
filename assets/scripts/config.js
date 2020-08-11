6b3ac754a6ceacc8eb74e3d91a130338d49b15ed
Dave-92-stack <dave.sher92@gmail.com>
2020-08-11 16:31:02 -0400

    Adjusted config settings

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://serene-sea-61439.herokuapp.com/',
+  production: 'https://serene-sea-61439.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
